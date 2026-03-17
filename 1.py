4df14041867dda7e82351283315c75e6f8de9a42
bogatyrevaek <bogatyrevaK2019@yandex.ru>
2026-03-17 21:07:33 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,26 +7,19 @@ parser = argparse.ArgumentParser(
     description="Merge into one file all files with given suffix whose size is less than specified."
 )
 
-parser.add_argument("suffix", help="file suffix")
-parser.add_argument("size", type=int, help="maximum file size in bytes")
-parser.add_argument("directory", help="directory to search")
-parser.add_argument("outfile", help="output file")
+parser.add_argument("-s", "--suffix", required=True, help="file suffix")
+parser.add_argument("-z", "--size", type=int, required=True, help="maximum file size in bytes")
+parser.add_argument("-d", "--directory", default=".", help="directory to search (default: current directory)")
+parser.add_argument("-o", "--output", required=True, help="output file")
 
 args = parser.parse_args()
 
-suffix = args.suffix
-size = args.size
-directory = args.directory
-outfile = args.outfile
-
-with open(outfile, "w") as out:
-    for root, dirs, files in os.walk(directory):
+with open(args.output, "w") as out:
+    for root, dirs, files in os.walk(args.directory):
         for name in files:
-            if name.endswith("." + suffix):
+            if name.endswith("." + args.suffix):
                 path = os.path.join(root, name)
-                filesize = os.path.getsize(path)
-
-                if filesize < size:
+                if os.path.getsize(path) < args.size:
                     print("adding", path)
                     with open(path, "r") as f:
                         out.write(f.read())
